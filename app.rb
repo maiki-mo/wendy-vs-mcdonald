d26367976b2e14b1f4ca33c2bc2cef9b32ebdaa4
maikimo <mike.p.monahan@gmail.com>
2018-04-25 00:18:29 -0400

    changed small stuff

--- app.rb
+++ app.rb
@@ -10,16 +10,16 @@ get '/' do
 end
 
 post '/' do
-  @email = params[:email]
-  @subject = params[:subject]
-  @message = params[:message]
-  from = Email.new(email: @email)
-  to = Email.new(email: 'mike.p.monahan@gmail.com')
-  subject = @subject
-  content = Content.new(type: 'text/plain', value: @message)
-  mail = Mail.new(from, subject, to, content)
+  # @email = params[:email]
+  # @subject = params[:subject]
+  # @message = params[:message]
+  # from = Email.new(email: @email)
+  # to = Email.new(email: 'mike.p.monahan@gmail.com')
+  # subject = @subject
+  # content = Content.new(type: 'text/plain', value: @message)
+  # mail = Mail.new(from, subject, to, content)
 
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
-  redirect '/'
+  # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  # response = sg.client.mail._('send').post(request_body: mail.to_json)
+  # redirect '/'
 end
