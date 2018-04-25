df68ee914cf0a883941036ef1a31ce6a867bfb08
maikimo <mike.p.monahan@gmail.com>
2018-04-24 23:53:36 -0400

    battle of the century

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+require 'sendgrid-ruby'
+
+include SendGrid
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @email = params[:email]
+  @subject = params[:subject]
+  @message = params[:message]
+  from = Email.new(email: @email)
+  to = Email.new(email: 'mike.p.monahan@gmail.com')
+  subject = @subject
+  content = Content.new(type: 'text/plain', value: @message)
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  redirect '/'
+end
